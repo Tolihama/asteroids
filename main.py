81837dad63825b2e2a589d8a3ba9fcfa73bff214
Francesco Marino <fra070@gmail.com>
2025-04-23 10:41:39 +0200

    chore: groups

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    game_loop(screen, clock, player)
+    game_loop(screen, clock, updatable, drawable)
 
-def game_loop(screen, clock, player):
+def game_loop(screen, clock, updatable, drawable):
     dt = 0
 
     while True:
@@ -25,9 +28,12 @@ def game_loop(screen, clock, player):
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
