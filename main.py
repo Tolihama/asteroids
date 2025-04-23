2c4aac494870af645afb115a8b59ce068f437509
Francesco Marino <fra070@gmail.com>
2025-04-23 10:13:25 +0200

    feat: game clock

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    game_loop(screen)
+    game_loop(screen, clock)
 
-def game_loop(screen):
+def game_loop(screen, clock):
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,6 +25,7 @@ def game_loop(screen):
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
